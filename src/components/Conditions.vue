777724c0d5dbafe9b7717b1df71fbb652c1b1229
Darren Shankland <ds@4pq.co.uk>
2019-05-04 18:44:05 +0100

    separate out current conditions

---
+++ src/components/Conditions.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div v-if="conditions" id="conditions">
+    <h1>{{conditions.city_name}}</h1>
+    <h2>Current Conditions</h2>
+    <h1>{{conditions.weather.description}}</h1>
+    <img v-bind:src="'https://www.weatherbit.io/static/img/icons/' + conditions.weather.icon + '.png'">
+    <p>sunrise: {{conditions.sunrise}}</p>
+    <p>sunset: {{conditions.sunset}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'current-conditions',
+  props: ['conditions']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
--- src/views/ForeCastView.vue
+++ src/views/ForeCastView.vue
@@ -1,21 +1,29 @@
 <template lang="html">
   <div id="view">
-    <h2> Forecast View </h2>
+    <Conditions v-if="currentConditions" :conditions="currentConditions.data[0]" />
+
 
   </div>
 </template>
 
 <script>
 import { apiKey } from '../config.js';
+import Conditions from '@/components/Conditions.vue';
+import Forecast from '@/components/Forecast.vue';
+import ForecastSearch from '@/components/ForecastSearch.vue';
 
 export default {
   name: 'forecast-view',
   components: {
-    apiKey
+    apiKey,
+    Conditions,
+    Forecast,
+    ForecastSearch
   },
   data() {
     return {
-      forecastData: null
+      currentConditions: null,
+      hourlyForecast: null
     };
   },
   methods: {
@@ -23,9 +31,13 @@ export default {
       fetch(`https://api.weatherbit.io/v2.0/current?ip=auto&key=${apiKey}`)
       .then(response => response.json())
       .then(data => {
-        this.forecastData = data;
-        console.log(data);
-      })
+        this.currentConditions = data;
+        fetch(`https://api.weatherbit.io/v2.0/forecast/hourly/?lat=${this.currentConditions.data[0].lat}&lon=${this.currentConditions.data[0].lon}&key=${apiKey}`)
+        .then(response => response.json())
+        .then(data =>{
+          this.hourlyForecast = data;
+        });
+      });
     },
   },
   mounted() {
