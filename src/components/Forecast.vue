174b328e42e888ffde9db0b0813fb1d5d8a849e0
Darren Shankland <ds@4pq.co.uk>
2019-05-04 21:34:30 +0100

    fix forecast divs

--- src/components/Forecast.vue
+++ src/components/Forecast.vue
@@ -2,7 +2,8 @@
   <div class="forecast">
     <h1>Hourly Forecast</h1>
     <div v-for="(hour, index) in forecast" class="hourly-forecast">
-      <h3>{{hour.timestamp_local}} {{hour.weather.description}} </h3>
+      <h4>{{hour.timestamp_local.slice(11)}}</h4>
+      <h4><br>{{hour.weather.description}}</h4>
       <img v-bind:src="'https://www.weatherbit.io/static/img/icons/' + hour.weather.icon + '.png'" width=80px>
     </div>
   </div>
@@ -18,20 +19,20 @@ export default {
 <style lang="css" scoped>
 
 .forecast {
-  display: inline-block;
   width: 100%;
   text-align: center;
   height: 300px;
   overflow-x: scroll;
   overflow-y: hidden;
+  white-space: nowrap;
 }
 
 .hourly-forecast {
-
-  float: left;
+  display: inline-block;
   border: 1px solid black;
   width: 120px;
   height: 210px;
+  vertical-align: middle;
 }
 
 </style>
